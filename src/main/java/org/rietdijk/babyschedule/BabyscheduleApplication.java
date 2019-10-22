12c337feb669b82769e647bdd0116954b60bfc3d
KingGizza <maaikerietdijk@hotmail.com>
2019-10-22 11:12:42 +0200

    Initial commit

---
+++ src/main/java/org/rietdijk/babyschedule/BabyscheduleApplication.java
@@ -0,0 +1,13 @@
+package org.rietdijk.babyschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyscheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyscheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rietdijk/babyschedule/BabyscheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rietdijk.babyschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyscheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
